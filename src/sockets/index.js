bcd0e34ec6f6de1057d888103fc8bbc5e508d967
HansM7 <melchorhans5@gmail.com>
2022-06-24 18:17:17 +0100

    model 02

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -4,6 +4,10 @@ const newProduct = new Product()
 const Chat = require('../chat/model')
 const newChat = new Chat()
 
+// const axios = require('axios').default
+
+const fetch = require('node-fetch')
+
 module.exports = (io) => {
 
     io.on('connection', (socket) => {
@@ -11,14 +15,33 @@ module.exports = (io) => {
         const sendProducts = async()=>{
             const products = await newProduct.getAll()
             io.emit("allProductsServer", products)
-            console.log(products);
         }
         sendProducts()
 
         socket.on('processAddProduct', async (data) => {
             console.log(data)
 
-            sendProducts()
+            // const res = await axios('/productos');
+            // io.emit("allProductsServer",res.json())
+            // axios.get('/productos')
+            // .then((response) => {  response.json() })
+            // .then((data) => { io.emit("allProductsServer",data) })
+
+            // io.emit("allProductsServer", [{
+            //     title:"ejemplo",
+            //     price:10,
+            //     thumbnail:"ejemplo"
+            // }])
+
+            // const products = await newProduct.getAll()
+            // io.emit("allProductsServer", products)
+            // console.log(products)
+
+            const data2 = await fetch("http://localhost:3000/productos")
+            const newdata = await data2.json()
+
+            io.emit("allProductsServer", newdata)
+
         })
 
         console.log('Un usuario se a conectado')
